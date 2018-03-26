1cf9a060e18423837290c18a7deb4cf56f22f7dc
Taylor Donlan <taylordonlan@gmail.com>
2018-03-26 21:06:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
 
   def execute_transaction
-    if !@sender.valid?
+    if !(@sender.valid?)
       "Transaction rejected.  Please check your account balance."
     else
     end
